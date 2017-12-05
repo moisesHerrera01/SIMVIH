0fe6a5c7a5a1d17c175c151dfa026edc0ae0dcea
Walter Lemus <walter.lemus@hotmail.com>
2017-12-05 13:31:21 -0600

    correción de jquery para los mensajes

--- application/views/footer_view.php
+++ application/views/footer_view.php
@@ -3,7 +3,6 @@
 	<p>Creado por SHLDevelop</p>
 </footer>
 
-<script src="<?= base_url('assets/javascripts/jquery-1.11.3.min.js') ?>"></script>
 <script src="<?= base_url('assets/semantic/dist/semantic.min.js') ?>"></script>
 <script src="<?= base_url('assets/javascripts/main.js') ?>"></script>
 
