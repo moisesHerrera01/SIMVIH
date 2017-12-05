b744262455d028f94f09f750ae619a652067a064
moisesHerrera01 <moises.oct@gmail.com>
2017-12-05 12:08:12 -0600

    funciona filtro para ARV
    - filtra por un ARV y por todos
    - pendiente filtros fechas y clinica

--- application/controllers/Reportes/Reporte_medicamentos.php
+++ application/controllers/Reportes/Reporte_medicamentos.php
@@ -12,19 +12,16 @@ class Reporte_medicamentos extends CI_Controller {
 
 		$this->load->model('Gestionar_antiretrovirales_model');
 
-		$compras_arv = false;
-		$compras_arvs = false;
-		$arv = false;
-		$consumido = false;
-		$consumidos = false;
+		$cantidades = false; //Array que contiene la información a mostrar de los ARV's	
+		$cantidad = false; //Array que contiene la información de un solo ARV
 		$arvs = $this->Gestionar_antiretrovirales_model->getAntiretroviral();
 
 
 		if ($this->input->get('fecha_i') && $this->input->get('fecha_f')) {
-			if ($this->input->get('arv')=='default') {
+			if ($this->input->get('arv')=='default') { //Caso para el cual no se especifica ARV
 				$json = 'http://localhost/restserver/index.php/compras/compras/format/json';
 				$compras_arvs  = json_decode(file_get_contents($json));
-				$consumido = array('numero'=>'','cantidad'=>'','comprado'=>'');
+				$consumido = array('numero'=>'','nombre'=>'','cantidad'=>'','comprado'=>'', 'abreviatura' => '');
 				$consumidos = array();
 				foreach ($arvs as $arv) {
 					$total =0;
@@ -33,6 +30,8 @@ class Reporte_medicamentos extends CI_Controller {
 						$total+=$st->getCantidad();
 					}
 					$consumido['numero'] = $arv->getNumero();
+					$consumido['nombre'] = $arv->getNombre();
+					$consumido['abreviatura'] = $arv->getAbreviatura();
 					$consumido['cantidad'] = $total;
 					$consumidos[] =$consumido;
 
@@ -51,26 +50,37 @@ class Reporte_medicamentos extends CI_Controller {
 				    $cantidades[] = $var; 				
 					}
 			}
-			}else{
-				$data_reporte = $this->Gestionar_antiretrovirales_model->getReporteMedicamentos($this->input->get('arv'));
+			}else{ //Caso para el cual se especifica un ARV
+				$cant = 0; //Cantidad de ARV comprado
+				$consumido_arv = 0; //Cantidad de ARV consumido
+				$cantidad = array();
 
+				$data_reporte = $this->Gestionar_antiretrovirales_model->getReporteMedicamentos($this->input->get('arv'));
 				$arv = $data_reporte['arv'];
-				$json = 'http://localhost/restserver/index.php/compras/compras/numero/'.$arv->getNumero().'/format/json';
+				$json = 'http://localhost/restserver/index.php/compras/compras/format/json';
 				$compras_arv  = json_decode(file_get_contents($json));
 				$arv_estados = $arv->getArvEstados();
 
 				foreach ($arv_estados as $st) {
-					$consumido += $st->getCantidad();
-				}			
+					$consumido_arv += $st->getCantidad();					
+				}
+				foreach ($compras_arv as $c) {
+					if ($c->numero_producto == $arv->getNumero()) {
+						$cant += $c->cantidad;
+					}					
+				}
+				$cantidad['numero'] = $arv->getNumero();
+				$cantidad['nombre'] = $arv->getNombre();
+				$cantidad['abreviatura'] = $arv->getAbreviatura();
+				$cantidad['cantidad'] = $consumido_arv;
+				$cantidad['comprado'] =	$cant;			
 			}
 		}		
 
         $data  = array(
-			'arvs' => $arvs,
-			'compras_arv'  => $compras_arv,
-			'compras_arvs'  => $compras_arvs,
-			'arv' => $arv,
-			'consumidos' => $consumidos
+			'arvs' => $arvs,	
+			'cantidad' => $cantidad,
+			'cantidades' => $cantidades
 		);
 		
 		$this->load->view('reportes/reporte_medicamentos_view', $data);
